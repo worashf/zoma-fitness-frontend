2cc0e1794be099b2a247885553076b596d22a3c7
Worash <uppert83@gmail.com>
2023-01-17 16:00:32 +0300

    Add home page

---
+++ src/page/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Info from '../../component/info/Info'
+import Video from '../../component/landing/Video'
+
+
+
+ const Home = () => {
+    return (
+        <div className ="container-fluid">
+        <Info />
+        <Video/>
+        </div>
+    )
+}
+
+export default Home;
