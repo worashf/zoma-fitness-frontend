99957883f3170c2af8044b5455beaee58988e800
Worash <uppert83@gmail.com>
2023-01-17 15:43:44 +0300

    Create Project list component

---
+++ src/Projects.js
@@ -0,0 +1,13 @@
+
+
+
+import React from 'react'
+const Projects = () => {
+    return (
+        <div>
+            <h1>Hello from projects</h1>
+        </div>
+    )
+}
+
+export default Projects;
