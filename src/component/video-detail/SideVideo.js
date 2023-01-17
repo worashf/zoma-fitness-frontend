04e6775fedf6c36796af5419a768a136f3a20c98
Worash <uppert83@gmail.com>
2023-01-17 15:59:07 +0300

    Add side video

---
+++ src/component/video-detail/SideVideo.js
@@ -0,0 +1,161 @@
+
+import React from 'react'
+
+
+import './videoSide.css'
+ const SideVideo = () => {
+    return (
+        <div className="side-video">
+            
+      
+        <div className="video__controls">
+          <p>Up Next</p>
+          <div className="autoplay">
+            <i className="fas fa-toggle-on"></i>
+            <p>AUTOPLAY</p>
+          </div>
+        </div>
+
+        <div className="video__container">
+          <div className="video-box">
+            <img src="assets/img/thumbnail.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+
+          <div className="video-box">
+            <img src="assets/img/thumbnail-1.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+
+          <div className="video-box">
+            <img src="assets/img/thumbnail-2.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+
+          <div className="video-box">
+            <img src="assets/img/thumbnail-3.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+
+          <div className="video-box">
+            <img src="assets/img/thumbnail-4.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+
+          <div className="video-box">
+            <img src="assets/img/thumbnail-5.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+
+          <div className="video-box">
+            <img src="assets/img/thumbnail-6.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+
+          <div className="video-box">
+            <img src="assets/img/thumbnail-7.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+          <div className="video-box">
+            <img src="assets/img/thumbnail-8.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+          <div className="video-box">
+            <img src="assets/img/thumbnail-9.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+          <div className="video-box">
+            <img src="assets/img/thumbnail-10.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+          <div className="video-box">
+            <img src="assets/img/thumbnail-11.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+       
+          <div className="video-box">
+            <img src="assets/img/thumbnail-13.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+          <div className="video-box">
+            <img src="assets/img/thumbnail-14.jpg" alt="Video thumbnail" className="video-box__thumbnail"/>
+            <div className="video-box__details">
+              <h3>'How to Get a Job at the Big 4 - Amazon, Facebook, Google & Microsoft' by
+                Sean Lee</h3>
+              <p>imtiana</p>
+              <p>969K views</p>
+            </div>
+          </div>
+
+        </div>
+ 
+
+        </div>
+    )
+}
+export default SideVideo;
