586f7d659677c7e03a5ba332e546249deaf540f4
Worash <uppert83@gmail.com>
2023-01-17 15:00:52 +0300

    Create home component

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import VideoDetailPage from './page/video-detail/VideoDetailPage'
+const Contact = () => {
+    return (
+        <div>
+            <h1>Hello from contact</h1>
+            <VideoDetailPage/>
+        </div>
+    )
+}
+
+export default Contact;
